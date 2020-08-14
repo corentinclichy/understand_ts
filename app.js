5b59b30df4464f16dfee14365e6ac4c1e362d316
Corentin Clichy <cor.clichy@gmail.com>
2020-08-14 12:06:00 +0200

    testing core type in TypeScript

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Your code come here!!!");
+function add(n1, n2, showResult, phrase) {
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 3.5;
+var printResult = true;
+var resultPhrase = "Result is: ";
+var result = add(number1, number2, printResult, resultPhrase);
+console.log(result);
